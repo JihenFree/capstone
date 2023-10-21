9056de336ede9b85ce3f8c942484e4cd74cf53ab
Jihen <jihenfree@gmail.com>
2023-10-21 22:13:34 +0200

    add path to wsgi.py

--- capstone/wsgi.py
+++ capstone/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'capstone.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'capstone.settings')
 
 application = get_wsgi_application()
