16c1eafc48126ee697c51bc902573bc604394b73
Jihen <jihenfree@gmail.com>
2023-10-23 20:19:37 +0200

    add gunicorn.wsgi in build.sh

--- build.sh
+++ build.sh
@@ -17,7 +17,12 @@ pip install dj-database-url
 pip install psycopg2-binary
 pip install 'whitenoise[brotli]'
 pip install gunicorn
+
 echo "Done"
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+echo "run gunicorn"
+gunicorn capstone.wsgi:application
+echo "run gunicorn Done"
